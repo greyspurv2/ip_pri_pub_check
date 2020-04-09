3f0c2d3413c39d227076b5be7b1122f7e5e1b21d
greyspurv2 <35030409+greyspurv2@users.noreply.github.com>
2020-04-10 01:31:16 +0200

    Create ip-checker.py
    tells if an ipv4 is private or public from range

---
+++ ip-checker.py
@@ -0,0 +1,26 @@
+def privateip(a, b, c):
+
+    a = range([10.0.0.0.0], stop[10.255.255.255.256]) 
+    b = range([172.16.0.0] stop[172.31.255.255.256])
+    c = range([192.168.0.0] stop[192.168.255.256])
+
+print("Enter the IP")
+if
+input(num) = a
+print("the IP is private")
+else:
+    print("the IP is public)
+
+print("Enter the IP")
+if
+input() = b
+else:
+    print("the IP is public)
+
+print("Enter the IP")
+if
+input() = class_a
+else:
+    print("the IP is public)
+
+
