192b566ddf1af874512e025321ab7ebfe4183f51
greyspurv2 <35030409+greyspurv2@users.noreply.github.com>
2020-04-10 01:39:16 +0200

    Update ip-checker.py

--- ip-checker.py
+++ ip-checker.py
@@ -1,12 +1,12 @@
 def privateip(a, b, c):
 
     a = range([10.0.0.0.0], stop[10.255.255.255.256]) 
-    b = range([172.16.0.0] stop[172.31.255.255.256])
-    c = range([192.168.0.0] stop[192.168.255.256])
+    b = range([172.16.0.0], stop[172.31.255.255.256])
+    c = range([192.168.0.0], stop[192.168.255.256])
 
 print("Enter the IP")
 if
-input(num) = a
+input() = a
 print("the IP is private")
 else:
     print("the IP is public)
